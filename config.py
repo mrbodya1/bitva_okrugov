24dbe8833f506da44490bbc4abd51e4da7fc71ec
Fedor Stroitelev <mr.bodya1@gmail.com>
2026-04-20 10:32:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 # Supabase
 SUPABASE_URL = "https://vuukyjlxhxcjvyjivwpq.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1dWt5amx4aHhjanZ5aml2d3BxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MzQzMjgsImV4cCI6MjA5MjIxMDMyOH0.J3UQ5XXOFh6_delb2y3T3Ci5SMIwzWsKbhp2wraPdus"
 
-# VK
+# VK (замените на свои)
 VK_GROUP_TOKEN = "vk1.a.D4r_yH58-9Hz0a06BugwqUjYF5zk_q8zo_pBGCj0up6SdvVdwX2YT9rfwhlTTrHDD9OVzZx-zYVqiEktaT3ojH1owHKnjHCmfdR7qq4tXGGJ0iXzmjISCAbOvNrg58BOyFJ_vlEHy1PD7vXtBuO_dkru6RSXUFqqeZlag00DUR2l0q-Omx_LI_3FKhgPaTJTpmqom3mycXXKL6Ww4DqReQ"
 VK_CONFIRMATION = "aeeda3b0"
 VK_CALLBACK_SECRET = "cH4ll3ng3_86vs89_K3y"
